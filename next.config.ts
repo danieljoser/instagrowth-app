2b6aa7c72144b98a2c34ab72bdd9cd8d549f6997
unknown <danieljoser95@gmail.com>
2024-11-07 19:18:50 -0400

    Changing to deploy

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks', '@mantine/form'],
     
   },
-  basePath: "/instagrowth-appgit",
+  basePath: "/InstagrowthApp",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
