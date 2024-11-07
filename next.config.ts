1cfd000bb012cc6b347a3c1984bf92f08f7d44a4
unknown <danieljoser95@gmail.com>
2024-11-07 19:38:39 -0400

    Changing base path

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks', '@mantine/form'],
     
   },
-  basePath: "/instagrowth-app",
+  basePath: "/",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
