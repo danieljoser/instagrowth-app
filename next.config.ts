0ad7ee9787038fea38560879e9f50fa4b01f0d5f
unknown <danieljoser95@gmail.com>
2024-11-07 18:42:33 -0400

    Setup to Github Pages

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks', '@mantine/form'],
     
   },
-  basePath: "/2048-in-react",
+  basePath: "/instagrowth-appgit",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
