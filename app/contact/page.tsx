d9fcf622e0280add3be30eec257637b1f0c7defe
unknown <danieljoser95@gmail.com>
2024-11-07 21:02:44 -0400

    Changing Image src link

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const Contact = () => {
   return (
-    <div className="py-32 px-32 bg-[url('/assets/images/background-1.png')] bg-cover bg-no-repeat bg-center flex flex-col items-center h-full gap-8">
+    <div className="py-32 px-32 bg-[url('assets/images/background-1.png')] bg-cover bg-no-repeat bg-center flex flex-col items-center h-full gap-8">
          <Image 
               src="assets/images/logo.png"
               width={400}
